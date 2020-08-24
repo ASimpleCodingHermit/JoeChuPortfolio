94729148a4b56a071e71dff6020545b6bb09c7ee
Joseph Chu <hotcupofjoe2013@gmail.com>
2020-08-24 01:09:32 -0400

    Added Bootstrap 4

--- home/views.py
+++ home/views.py
@@ -4,20 +4,21 @@ from django.shortcuts import render, HttpResponse
 
 
 def home(request):
-    return HttpResponse('this is my home page (/)')
+    context = {"name": "Joe", "jobTitle": "Full Stack Developer"}
+    return render(request, 'home.html', context)
 
 
 def about(request):
-    return HttpResponse('this is my about page (/about)')
+    return render(request, 'about.html')
 
 
 def projects(request):
-    return HttpResponse('this is my projects page (/projects)')
+    return render(request, 'projects.html')
 
 
 def blog(request):
-    return HttpResponse('this is my blog page (/blog)')
+    return render(request, 'blog.html')
 
 
 def contact(request):
-    return HttpResponse('this is my contact page (/contact)')
+    return render(request, 'contact.html')
